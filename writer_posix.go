7a84981c1218a2252ad0d42baebdc9d535aaef33
Javier Escalada <kerrigan29a@gmail.com>
2019-01-31 16:28:54 +0100

    Clear the last line after going up

--- writer_posix.go
+++ writer_posix.go
@@ -11,4 +11,5 @@ func (w *Writer) ClearLines() {
 		fmt.Fprintf(w.Out, "%c[2K", ESC)     // clear the line
 		fmt.Fprintf(w.Out, "%c[%dA", ESC, 1) // move the cursor up
 	}
+	fmt.Fprintf(w.Out, "%c[2K", ESC) // clear the line
 }
